ca6c4ae784bf2ab7ca21b8dbfc3bebcc3d6f1d55
Sierra!~ <48463140+9u3@users.noreply.github.com>
2024-05-02 05:20:06 +0100

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,6 @@
+-- the actual. yk. loader
+-- more scripts will be added (eventually)
+
+if _G.PlaceID == 7041939546 then -- Catalog Avatar Creator
+    loadstring(Game:HttpGet("https://github.com/9u3/roblox/raw/main/scripts/".. _G.PlaceID ..".lua"))
+end
