6de7931d0a07730e705b457839fb2c1d8f0834c8
Sierra!~ <48463140+9u3@users.noreply.github.com>
2024-05-03 01:09:32 +0100

    Create old.lua

---
+++ old.lua
@@ -0,0 +1,7 @@
+-- the actual. yk. loader
+-- this one will be updated less.
+-- more scripts will be added (eventually)
+print(_G.PlaceID)
+if _G.PlaceID == 7041939546 then -- Catalog Avatar Creator
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/9u3/roblox/main/scripts/".. _G.PlaceID ..".lua"))
+end
