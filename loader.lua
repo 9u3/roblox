d19e9a2172e684282e08820c1a479bcc61f973dd
Sierra!~ <48463140+9u3@users.noreply.github.com>
2024-05-03 06:31:33 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,4 +4,11 @@
 print(_G.PlaceID)
 if _G.PlaceID == 7041939546 then -- Catalog Avatar Creator
     loadstring(game:HttpGet("https://raw.githubusercontent.com/9u3/roblox/main/scripts/".. _G.PlaceID ..".lua", true))()
+elseif _G.PlaceID == 2788229376 then -- Da Hood
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/9u3/roblox/main/scripts/".. _G.PlaceID ..".lua", true))()
+elseif _G.PlaceID == 7213786345 then -- Da Hood VC
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/9u3/roblox/main/scripts/2788229376.lua", true))()
+elseif _G.PlaceID == 16033173781 then -- Da Hood Macro
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/9u3/roblox/main/scripts/2788229376.lua", true))()
 end
+
